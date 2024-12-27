2723b960c639aaeebf6e7922fcf7c153fb901ed5
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-12-27 19:20:18 +0100

    Initialize User class with arguments

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
+import uuid
 from extensions import db
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash
 
 
 class User(db.Model, UserMixin):
@@ -7,6 +9,12 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(80), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, username=None, email=None, password=None):
+        self.id = uuid.uuid4()
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
 
     def __repr__(self):
         return f"<User {self.username}>"
