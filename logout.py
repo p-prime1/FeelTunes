fdf1a822ddbd452016992991adb0accc83015435
innocent <terminalkarabo@gmail.com>
2024-12-28 10:58:38 +0200

    Implement logout session clearing, enhance flash message styles

--- logout.py
+++ logout.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, flash
+from flask import Blueprint, redirect, url_for, flash, session
 from flask_login import logout_user
 
 # Define the blueprint
@@ -7,5 +7,6 @@ logout_bp = Blueprint('logout', __name__)
 @logout_bp.route('/logout', methods=['GET'])
 def logout():
     logout_user()
+    session.clear()
     flash('You have been logged out.', 'success')
     return redirect(url_for('login.login'))
