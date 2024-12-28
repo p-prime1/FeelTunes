f2fa4201eee91f32dc0983f8dbfdf0f7cd9e85f9
innocent <terminalkarabo@gmail.com>
2024-12-28 17:11:17 +0200

    Add terms of service agreement checkbox to registration form and update .gitignore

--- register.py
+++ register.py
@@ -18,6 +18,11 @@ def register():
         email = form.email.data
         password = form.password.data
         
+        # Check if the "accept_terms" checkbox is checked
+        accept_terms = request.form.get('accept_terms')
+        if not accept_terms:
+            flash("You must agree to the Terms of Service to register.", "danger")
+            return redirect(url_for('register.register'))
         
         
         # Check if the username or email already exists
