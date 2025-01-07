720fa14bf0a7ec5f473fbaf4f1f4307d9c93f389
innocent <terminalkarabo@gmail.com>
2025-01-07 23:39:20 +0200

    add gunicorn to requirements and configure app to run on specified port

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from app import create_app, db
 from models import User
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     with app.app_context():
         db.create_all()  # Create database tables
-    app.run()
+    app.run(host="0.0.0.0", port=port)
