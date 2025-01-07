22c4b059b9e77b54dec112c52ce36329e9cbbae3
innocent <terminalkarabo@gmail.com>
2025-01-07 13:07:17 +0200

    add empty flask session file

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-from app import create_app
+from app import create_app, db
 from models import User
 
 app = create_app()
 
 if __name__ == "__main__":
-# Create database tables
+    with app.app_context():
+        db.create_all()  # Create database tables
     app.run(debug=True)
