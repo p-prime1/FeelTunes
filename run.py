fc26cdc78df21578cdeb88b3b6c7f16bf466efa8
innocent <terminalkarabo@gmail.com>
2025-01-08 00:38:15 +0200

    initialize database tables within app context and update requirements for Flask-SQLAlchemy and Werkzeug

--- run.py
+++ run.py
@@ -6,6 +6,4 @@ app = create_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    with app.app_context():
-        db.create_all()  # Create database tables
     app.run(host="0.0.0.0", port=port)
